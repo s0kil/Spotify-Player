9f24a3c87b472f0bf679a1e69208d4eecdfa327a
Daniel Sokil <dtechtours@gmail.com>
2019-12-25 00:26:19 -0500

    Load Featured Items onMount

--- src/routes/home/Home.svelte
+++ src/routes/home/Home.svelte
@@ -1,24 +1,31 @@
 <script>
+  import {onMount} from "svelte";
+
   import SpotifyService from "../../services/spotify";
 
   import SpotifyLogin from "../../components/SpotifyLogin.svelte";
 
-  let playlists = SpotifyService.getFeaturedPlaylists();
+  let playlists;
+  onMount(() => {
+    playlists = SpotifyService.getFeaturedPlaylists();
+  });
 </script>
 
 <SpotifyLogin/>
 
-{#await playlists}
-  <p>Loading Featured Playlists</p>
-{:then playlistsData}
-    {#each playlistsData.playlists.items as playlist}
-      <p>{playlist.name}</p>
-      <p>{@html playlist.description}</p>
-      <img src="{playlist.images[0].url}" alt="{playlist.name}">
-      <hr>
-    {/each}
-{:catch error}
-  <h1>Error</h1>
-  <code>{error.response}</code>
-  {console.error(error)}
-{/await}
+{#if playlists}
+    {#await playlists}
+      <p>Loading Featured Playlists</p>
+    {:then playlistsData}
+        {#each playlistsData.playlists.items as playlist}
+          <p>{playlist.name}</p>
+          <p>{@html playlist.description}</p>
+          <img src="{playlist.images[0].url}" alt="{playlist.name}">
+          <hr>
+        {/each}
+    {:catch error}
+      <h1>Error</h1>
+      <code>{error.response}</code>
+      {console.error(error)}
+    {/await}
+{/if}
